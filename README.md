5962469c3149a37a1f5fdca9069f5f62b18b35ca
Dakota Madden-Fong <dmaddenf@willamette.edu>
2019-05-17 21:07:49 -0700

    added initial files and the tracking.js library

---
+++ README.md
@@ -0,0 +1,2 @@
+# WebGL-Game
+An excercise to learn javascript, three.js and webgl
---
+++ http_server.bat
@@ -0,0 +1,4 @@
+start http://localhost:8000/
+start notepad++ script.js
+start notepad++ index.html
+python -m http.server
