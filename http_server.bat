5bbae85aec2fcc741873d4216f181b03cc94393e
Dakota Madden-Fong <dmaddenf@willamette.edu>
2019-05-18 20:04:44 -0700

    lots (see description)
    1. Added webcam code to display webcam in page
    
    2. Added button to find features and display on page from frame of cam

--- http_server.bat
+++ http_server.bat
@@ -1,4 +1,5 @@
 start http://localhost:8000/
 start notepad++ script.js
 start notepad++ index.html
+start notepad++ style.css
 python -m http.server 8000
---
+++ script.js
@@ -0,0 +1,47 @@
+window.onload = function() {
+	
+	//Load Hidden Image from File
+	var img = new Image();  
+	img.src = 'test_web/cct_ima.png'
+	img.onload = function(){
+		doFindFeatures();
+	}
+	
+	//Video Feed From Webcam
+	var webcam_feed = document.getElementById("webcam_feed");
+	
+	if (navigator.mediaDevices.getUserMedia) {
+		navigator.mediaDevices.getUserMedia({ video: true })
+			.then(function (stream) {
+				webcam_feed.srcObject = stream;
+			})
+			.catch(function (err0r) {
+				console.log("Something went wrong!");
+			});
+	}
+	
+	
+	var width = 500;
+	var height = 375;
+	var canvas = document.getElementById('canvas');
+	var context = canvas.getContext('2d');
+	//var image = document.getElementById('image');
+	
+	var doFindFeatures = function() {
+		console.log('hi');
+		tracking.Fast.THRESHOLD = 25;
+		context.drawImage(webcam_feed, 0, 0, width, height);
+		var imageData = context.getImageData(0, 0, width, height);
+		var gray = tracking.Image.grayscale(imageData.data, width, height);
+		var corners = tracking.Fast.findCorners(gray, width, height);
+		for (var i = 0; i < corners.length; i += 2) {
+			context.fillStyle = '#ff00fa';
+			context.fillRect(corners[i], corners[i + 1], 3, 3);
+		}
+	};
+	//doFindFeatures();
+	
+	//Button to Process Frame of Webcam Video
+	scan_button = document.getElementById('scan_button');
+	scan_button.onclick = doFindFeatures;
+}
