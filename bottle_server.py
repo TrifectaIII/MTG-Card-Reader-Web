73fc8cfcdda824655d69b802a4884038ef4e1b33
Dakota Madden-Fong <dmaddenf@willamette.edu>
2019-05-27 08:34:50 -0700

    formatting

--- bottle_server.py
+++ bottle_server.py
@@ -11,18 +11,18 @@ os.chdir(dname)
 
 
 ##################################################################################
-    
-    
+
+
 # Accept Request for Set Load
-@route('/load_set', method = 'POST')
+@route('/load_set', method='POST')
 def load_set():
-    print(request.body.read())# has form of bytestring
+    print(request.body.read())  # has form of bytestring
     return "load_set not fully implemented yet"
 
 # Accept Request for Card Match
-@route('/match_card', method = 'POST')
+@route('/match_card', method='POST')
 def match_card():
-    cam_png_uri = request.body.read()# Read body of post request
+    cam_png_uri = request.body.read()  # Read body of post request
     card_name = matching.match(cam_png_uri)
     return card_name
 
@@ -38,9 +38,9 @@ def index():
 # Serve Static Files
 @route('/<filepath:path>')
 def send_static(filepath):
-    return static_file(filepath,root='.')
-    
-    
+    return static_file(filepath, root='.')
+
+
 ##################################################################################
 
 
