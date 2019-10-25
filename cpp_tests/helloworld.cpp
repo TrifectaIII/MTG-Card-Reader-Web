aac21d9145f075582106250b97b0e4cba8bb7419
Dakota Madden-Fong <maddenfong@gmail.com>
2019-10-25 15:57:51 -0700

    added very basic setup for c++ testing

---
+++ cpp_tests/helloworld.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+    vector<string> msg {"Hello", "C++", "World", "from", "VS Code!"};
+
+    for (const string& word : msg)
+    {
+        cout << word << " ";
+    }
+    cout << endl;
+}
